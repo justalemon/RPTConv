e41c510f7f42041a97cfe4ededc762371d1ff56c
Hannele Ruiz <justlemoncl@gmail.com>
2025-05-13 01:18:01 -0400

    Added basic fetching of the file itself

--- rptconv.py
+++ rptconv.py
@@ -1,8 +1,23 @@
+from pathlib import Path
+
+import requests
 import typer
 
 
-def main():
+
+def process_excel(contents: str):
     pass
+
+
+def main(file: str = None):
+    if file is None:
+        request = requests.get("https://www.subtel.gob.cl/wp-content/uploads/2025/03/Informes_RA_26_03_2025_Repetidoras.xlsx")
+        request.raise_for_status()
+        content = request.content
+    else:
+        content = Path(file).read_bytes()
+
+    process_excel(content)
 
 
 if __name__ == "__main__":
